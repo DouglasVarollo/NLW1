24bbc223b40365a55334022250827c43f343fb0d
Douglas Varollo <dcvarollo@gmail.com>
2020-06-04 21:47:11 -0300

    Adiciona o mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.1.106:3333/uploads/${item.image}`,
       };
     });
 
